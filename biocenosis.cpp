9e46cd5a93874afcb7699e0ad49b8e48880580be
MovsisyanSas <sasunmov07@gmail.com>
2023-06-15 22:48:40 +0400

    biocenosis introduced with inheritance

--- biocenosis.cpp
+++ biocenosis.cpp
@@ -30,7 +30,7 @@ public:
 	std::string caviar;
 	fish() {
 		FishAbility = ",live in water longer,swim faster,breath under water";
-		gills = "gills";
+		gills = ",gills";
 		scale = ",scale";
 		slime = ",slime";
 		caviar = ",caviar";
@@ -51,7 +51,7 @@ public:
 	std::string hair;
 	LandDweller() {
 		LandDwellerAbility= ",walk better,more flexible,smarter,wilder";
-		teeth = "teeth";
+		teeth = ",teeth";
 		legs = ",legs";
 		hand = ",hand";
 		hair = ",hair";
@@ -72,6 +72,10 @@ public:
 	std::string beak;
 	bird() {
 		BirdAbility = ",fly,more careful,very fast, good vision";
+		wing = ",wing";
+		legs = ",legs";
+		feather = ",feather";
+		beak = ",beak";
 	}
 	void display() {
 		std::cout << ability << BirdAbility << std::endl;
@@ -96,65 +100,81 @@ public:
 class shark : public fish {
 public:
 	std::string sharkAbility;
+	std::string teeth;
+	std::string hugeTail;
 	shark() {
 		sharkAbility = ",wild,sharp teeth, good smelt of blood, huge, very fast under water";
+		teeth = ",teeth";
+		hugeTail = ",huge tail";
 	}
 	void display() {
 		std::cout << ability << FishAbility << sharkAbility << std::endl;
 		std::cout << std::endl;
-		std::cout << "parts of body: " << heart << eye << mouth << ears << gills << scale << caviar << std::endl;
+		std::cout << "parts of body: " << heart << eye << mouth << ears << gills << scale << caviar << teeth << hugeTail << std::endl;
 	}
 };
 
 class lion : public LandDweller {
 public:
 	std::string lionAbility;
+	std::string milk;
+	std::string muscles;
 	lion() {
 		lionAbility = ",strongest mammal, brave, orange";
+		milk = ",milk";
+		muscles = ",muscles";
 	}
 	void display() {
 		std::cout << ability << LandDwellerAbility << lionAbility << std::endl;
 		std::cout << std::endl;
-		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << std::endl;
+		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << milk << muscles << std::endl;
 	}
 };
 
 class rabbit : public LandDweller {
 public:
 	std::string rabbitAbility;
+	std::string wool;
+	std::string longTeeth;
 	rabbit() {
 		rabbitAbility = ",very flexible, small, hides good in snow, likes carrot";
+		wool = ",wool";
+		longTeeth = ",long teeth";
 	}
 	void display() {
 		std::cout << ability << LandDwellerAbility << rabbitAbility << std::endl;
 		std::cout << std::endl;
-		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << std::endl;
+		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << wool << longTeeth << std::endl;
 	}
 };
 
 class eagle : public bird {
 public:
 	std::string eagleAbility;
+	std::string claw;
 	eagle() {
 		eagleAbility = ",wild, flies high, hunts very good, can take heavy things";
+		claw = ",claw";
 	}
 	void display() {
 		std::cout << ability << BirdAbility << eagleAbility << std::endl;
 		std::cout << std::endl;
-		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << std::endl;
+		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << claw << std::endl;
 	}
 };
 
 class parrot : public bird {
 public:
 	std::string parrotAbility;
+	std::string colorfulWool;
 	parrot() {
 		parrotAbility = ",colorful, friendly, speaks in human lanuages, funny";
+		colorfulWool = ",colorfulWool";
 	}
 	void display() {
 		std::cout << ability << BirdAbility << parrotAbility << std::endl;
 		std::cout << std::endl;
-		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << std::endl;
+		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << colorfulWool << std::endl;
 	}
 };
 
