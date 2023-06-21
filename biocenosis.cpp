978c79915fa7d978c140a1cc5df79cd4c762c585
MovsisyanSas <sasunmov07@gmail.com>
2023-06-22 01:08:12 +0400

    abstract class addition

--- biocenosis.cpp
+++ biocenosis.cpp
@@ -2,42 +2,19 @@
 
 class animal {
 public:
-	std::string ability;
-	std::string heart;
-	std::string eye;
-	std::string mouth;
-	std::string ears;
-	std::string movement;
-	animal() {
-		ability = "can breath,eat,drink,live,move";
-		heart = "heart";
-		eye = ",eye";
-		mouth = ",mouth";
-		ears = ",ears";
-		movement = "by limbs";
-	}
+	std::string ability = "can breath,eat,drink,live,move";
+	std::string heart = "heart";
+	std::string eye = ",eye";
+	std::string mouth = ",mouth";
+	std::string ears = ",ears";
+	std::string movement = "by limbs";
+
 	virtual void display() {
 		std::cout << ability << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << std::endl;
 	}
-	virtual void move() {
-		std::cout << movement << std::endl;
-	}
-	animal(const animal& obj) {
-		this->ability = obj.ability;
-		this->heart = obj.heart;
-		this->eye = obj.eye;
-		this->mouth = obj.mouth;
-		this->ears = obj.ears;
-	}
-	animal& operator = (const animal& obj) {
-		this->ability = obj.ability;
-		this->heart = obj.heart;
-		this->eye = obj.eye;
-		this->mouth = obj.mouth;
-		this->ears = obj.ears;
-	}
+	virtual void move() = 0;
 	virtual ~animal() {
 		std::cout << "animal class destructor" << std::endl;
 	}
@@ -45,42 +22,20 @@ public:
 
 class fish : public animal {
 public:
-	std::string FishAbility;
-	std::string gills;
-	std::string scale;
-	std::string slime;
-	std::string caviar;
-	std::string FishMovement;
-	fish() {
-		FishAbility = ",live in water longer,swim faster,breath under water";
-		gills = ",gills";
-		scale = ",scale";
-		slime = ",slime";
-		caviar = ",caviar";
-		FishMovement = "swimming";
-	}
+	std::string FishAbility = ",live in water longer,swim faster,breath under water";
+	std::string gills = ",gills";
+	std::string scale = ",scale";
+	std::string slime = ",slime";
+	std::string caviar = ",caviar";
+	std::string FishMovement = "swimming";
+
 	void display() override {
 		std::cout << ability << FishAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << gills << scale << caviar << std::endl;
 	}
-	void move() override { 
-		std::cout << FishMovement << std::endl;
-	}
-	fish(const fish& obj) {
-		this->gills = obj.gills;
-		this->scale = obj.scale;
-		this->slime = obj.slime;
-		this->caviar = obj.caviar;
-		this->FishAbility = obj.FishAbility;
-	}
-	fish& operator = (const fish& obj) {
-		this->gills = obj.gills;
-		this->scale = obj.scale;
-		this->slime = obj.slime;
-		this->caviar = obj.caviar;
-		this->FishAbility = obj.FishAbility;
-	}
+	void move() override = 0;
+
 	~fish() override {
 		std::cout << "fish class destructor" << std::endl;
 	}
@@ -88,42 +43,20 @@ public:
 
 class LandDweller : public animal {
 public:
-	std::string LandDwellerAbility;
-	std::string teeth;
-	std::string legs;
-	std::string hand;
-	std::string hair;
-	std::string LDMovement;
-	LandDweller() {
-		LandDwellerAbility= ",walk better,more flexible,smarter,wilder";
-		teeth = ",teeth";
-		legs = ",legs";
-		hand = ",hand";
-		hair = ",hair";
-		LDMovement = "walking/running/jumping";
-	}
+	std::string LandDwellerAbility = ",walk better,more flexible,smarter,wilder";
+	std::string teeth = ",teeth";
+	std::string legs = ",legs";
+	std::string hand = ",hand";
+	std::string hair = ",hair";
+	std::string LDMovement = "walking/running/jumping";
+
 	void display() override {
 		std::cout << ability << LandDwellerAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << std::endl;
 	}
-	void move() override {
-		std::cout << LDMovement << std::endl;
-	}
-	LandDweller(const LandDweller& obj) {
-		this->LandDwellerAbility = obj.LandDwellerAbility;
-		this->teeth = obj.teeth;
-		this->legs = obj.legs;
-		this->hand = obj.hand;
-		this->hair = obj.hair;
-	}
-	LandDweller& operator = (const LandDweller& obj) {
-		this->LandDwellerAbility = obj.LandDwellerAbility;
-		this->teeth = obj.teeth;
-		this->legs = obj.legs;
-		this->hand = obj.hand;
-		this->hair = obj.hair;
-	}
+	void move() override = 0;
+
 	~LandDweller() override {
 		std::cout << "LandDweller class destructor" << std::endl;
 	}
@@ -131,42 +64,20 @@ public:
 
 class bird : public animal {
 public:
-	std::string BirdAbility;
-	std::string wing;
-	std::string legs;
-	std::string feather;
-	std::string beak;
-	std::string BirdMovement;
-	bird() {
-		BirdAbility = ",fly,more careful,very fast, good vision";
-		wing = ",wing";
-		legs = ",legs";
-		feather = ",feather";
-		beak = ",beak";
-		BirdMovement = "Flying";
-	}
+	std::string BirdAbility = ",fly,more careful,very fast, good vision";
+	std::string wing = ",wing";
+	std::string legs = ",legs";
+	std::string feather = ",feather";
+	std::string beak = ",beak";
+	std::string BirdMovement = "Flying";
+
 	void display() override {
 		std::cout << ability << BirdAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << std::endl;
 	}
-	void move() override {
-		std::cout << BirdMovement << std::endl;
-	}
-	bird(const bird& obj) {
-		this->BirdAbility = obj.BirdAbility;
-		this->wing = obj.wing;
-		this->legs = obj.legs;
-		this->feather = obj.feather;
-		this->beak = obj.beak;
-	}
-	bird& operator = (const bird& obj) {
-		this->BirdAbility = obj.BirdAbility;
-		this->wing = obj.wing;
-		this->legs = obj.legs;
-		this->feather = obj.feather;
-		this->beak = obj.beak;
-	}
+	void move() override = 0;
+
 	~bird() override {
 		std::cout << "bird class destructor" << std::endl;
 	}
@@ -180,12 +91,12 @@ public:
 		ishxanAbility = ",lives in Sevan, endemic, red colored";
 		IshxanMovement = "swimming";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << FishAbility << ishxanAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << gills << scale << caviar << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << IshxanMovement << std::endl;
 	}
 	ishxan(const ishxan& obj) {
@@ -194,7 +105,7 @@ public:
 	ishxan& operator = (const ishxan& obj) {
 		this->ishxanAbility = obj.ishxanAbility;
 	}
-	~ishxan() final {
+	~ishxan() override final {
 		std::cout << "ishxan class destructor" << std::endl;
 	}
 };
@@ -211,12 +122,12 @@ public:
 		hugeTail = ",huge tail";
 		SharkMovement = "swimming";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << FishAbility << sharkAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << gills << scale << caviar << teeth << hugeTail << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << SharkMovement << std::endl;
 	}
 	shark(const shark& obj) {
@@ -229,7 +140,7 @@ public:
 		this->teeth = obj.teeth;
 		this->hugeTail = obj.hugeTail;
 	}
-	~shark() final {
+	~shark() override final {
 		std::cout << "shark class destructor" << std::endl;
 	}
 };
@@ -246,12 +157,12 @@ public:
 		muscles = ",muscles";
 		LionMovement = "walking/running/jumping";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << LandDwellerAbility << lionAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << milk << muscles << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << LionMovement << std::endl;
 	}
 	lion(const lion& obj) {
@@ -264,7 +175,7 @@ public:
 		this->milk = obj.milk;
 		this->muscles = obj.muscles;
 	}
-	~lion() final {
+	~lion() override final {
 		std::cout << "lion class destructor" << std::endl;
 	}
 };
@@ -281,12 +192,12 @@ public:
 		longTeeth = ",long teeth";
 		RabbitMovement = "walking/running/jumping";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << LandDwellerAbility << rabbitAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << teeth << legs << hand << hair << wool << longTeeth << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << RabbitMovement << std::endl;
 	}
 	rabbit(const rabbit& obj) {
@@ -299,7 +210,7 @@ public:
 		this->wool = obj.wool;
 		this->longTeeth = obj.longTeeth;
 	}
-	~rabbit() final {
+	~rabbit() override final {
 		std::cout << "rabbit class destructor" << std::endl;
 	}
 };
@@ -314,12 +225,12 @@ public:
 		claw = ",claw";
 		EagleMovement = "Flying";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << BirdAbility << eagleAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << claw << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << EagleMovement << std::endl;
 	}
 	eagle(const eagle& obj) {
@@ -330,7 +241,7 @@ public:
 		this->eagleAbility = obj.eagleAbility;
 		this->claw = obj.claw;
 	}
-	~eagle() final {
+	~eagle() override final {
 		std::cout << "eagle class destructor" << std::endl;
 	}
 };
@@ -345,12 +256,12 @@ public:
 		colorfulFeather = ",colorful feather";
 		ParrotMovement = "Flying";
 	}
-	void display() final {
+	void display() override final {
 		std::cout << ability << BirdAbility << parrotAbility << std::endl;
 		std::cout << std::endl;
 		std::cout << "parts of body: " << heart << eye << mouth << ears << wing << legs << feather << beak << colorfulFeather << std::endl;
 	}
-	void move() final {
+	void move() override final {
 		std::cout << ParrotMovement << std::endl;
 	}
 	parrot(const parrot& obj) {
@@ -361,15 +272,13 @@ public:
 		this->parrotAbility = obj.parrotAbility;
 		this->colorfulFeather = obj.colorfulFeather;
 	}
-	~parrot() final {
+	~parrot() override final {
 		std::cout << "parrot class destructor" << std::endl;
 	}
 };
 
 int main() {
-	animal* Bird = new bird;
-	animal* Land = new LandDweller;
-	animal* Fish = new fish;
+
 	animal* Ishxan = new ishxan;
 	animal* Shark = new shark;
 	animal* Lion = new lion;
@@ -377,18 +286,16 @@ int main() {
 	animal* Parrot = new parrot;
 	animal* Eagle = new eagle;
 
-	animal** arr = new animal * [9];
-	arr[0] = Bird;
-	arr[1] = Land;
-	arr[2] = Fish;
-	arr[3] = Ishxan;
-	arr[4] = Shark;
-	arr[5] = Lion;
-	arr[6] = Rabbit;
-	arr[7] = Parrot;
-	arr[8] = Eagle;
+	animal** arr = new animal * [6];
 
-	for (int i = 0; i < 9; i++)
+	arr[0] = Ishxan;
+	arr[1] = Shark;
+	arr[2] = Lion;
+	arr[3] = Rabbit;
+	arr[4] = Parrot;
+	arr[5] = Eagle;
+
+	for (int i = 0; i < 6; i++)
 	{
 		arr[i]->display();
 		std::cout << std::endl;
